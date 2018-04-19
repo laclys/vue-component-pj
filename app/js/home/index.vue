d2fd89ce09b972fe2386c5b14e992d8220e45019
Lac <laclys@126.com>
2018-04-20 01:49:40 +0800

    add header

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,18 +1,14 @@
 <template lang="html">
-    <div :class="$style.home">
-        <h1>Home</h1>
-    </div>
+  <div>
+    <Header />
+  </div>
 </template>
 
 <script>
+import Header from '../public/header.vue'
 export default {
+  components: {
+    Header
+  }
 }
 </script>
-
-<style lang="scss" module>
-  @import '../../css/reset.scss';
-  .home{
-    color: red;
-    font-size: 80px;
-  }
-</style>
