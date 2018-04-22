7986889d337f478f590b78041d8b78acb268f369
Lac <laclys@126.com>
2018-04-22 18:54:51 +0800

    add component novice

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -2,16 +2,20 @@
   <div>
     <Header />
     <Slider />
+    <Novice />
   </div>
 </template>
 
 <script>
 import Header from '../public/header.vue'
 import Slider from "./hslider.vue"
+import Novice from './novice.vue'
+
 export default {
   components: {
     Header,
-    Slider
+    Slider,
+    Novice
   }
 }
 </script>
