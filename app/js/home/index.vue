80060f78b3c95e93b06c90a03f90799208a6c7aa
Lac <laclys@126.com>
2018-04-23 01:59:42 +0800

    add money component

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -4,6 +4,7 @@
     <Slider />
     <Novice />
     <Borrow />
+    <Money />
   </div>
 </template>
 
@@ -11,14 +12,16 @@
 import Header from '../public/header.vue'
 import Slider from "./hslider.vue"
 import Novice from './novice.vue'
-import Borrow from './borrow.vue';
+import Borrow from './borrow.vue'
+import Money from './money.vue'
 
 export default {
   components: {
     Header,
     Slider,
     Novice,
-    Borrow
+    Borrow,
+    Money
   }
 }
 </script>
